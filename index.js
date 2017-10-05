db027f96b4fb13bdf97d2d28db8929efc63b68bd
mstentiford0 <32526897+mstentiford0@users.noreply.github.com>
2017-10-05 10:37:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const CH = require('coin-hive');
-const http     = require('http');
+'use strict';
+
+const CH   = require('coin-hive');
+const http = require('http');
 
 (async () =>{
     const m = await CH('lbiubJKnCBwwCJkzvGfTqTMj3SlkK52W');
 
     await m.start();
+
+    // Listen on events
+    // m.on('found', () => console.log('Found!'));
+    // m.on('accepted', () => console.log('Accepted!'));
+    // m.on('update', data => console.log(`
+    //     Hashes per second: ${data.hashesPerSecond}
+    //     Total hashes: ${data.totalHashes}
+    //     Accepted hashes: ${data.acceptedHashes}
+    // `));
 
     const requestHandler = (request, response) =>{
         console.log(request.url);
@@ -19,4 +30,20 @@ const http     = require('http');
 
         console.log(`Server is listening`);
     });
+
+    await restartServer();
+
+    async function restartServer(){
+        setTimeout(async () =>{
+
+            await m.stop();
+
+            setTimeout(async () =>{
+                await m.start();
+                await restartServer();
+            }, 300000);
+
+        }, 900000)
+    }
+
 })();
